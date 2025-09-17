cb3e6fbeefcc0ca39c5a5432d7282dee4b02049b
daniloxn <daniloaugusto654@hotmail.com>
2025-09-17 18:24:03 -0300

    conta poupança e conta corrente

---
+++ src/ContaCorrente.java
@@ -0,0 +1,16 @@
+public class ContaCorrente extends Conta{
+    public ContaCorrente(Cliente cliente) {
+        super(cliente);
+    }
+
+    public void aplicarTarifaMensal() {
+        double tarifa = 10.0;
+        if (getSaldo() >= tarifa) {
+            setSaldo(getSaldo() - tarifa);
+            System.out.println("Tarifa mensal de R$ " + tarifa + "debitada .");
+
+        } else {
+            System.out.println("Saldo insuficiente para tarifa mensal.");
+        }
+    }
+}
---
+++ src/ContaPoupanca.java
@@ -0,0 +1,11 @@
+public class ContaPoupanca extends Conta {
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    public void aplicarRendimento() {
+        double rendimento = getSaldo() * 0.01;
+        setSaldo(getSaldo() + rendimento);
+        System.out.println("Rendimento de R$ " + rendimento + " aplicado.");
+    }
+}
